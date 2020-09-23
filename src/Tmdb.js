5293b63a4567c47e0583cb69bf69eb75e8650c96
analopesdev <analopesdemorais@outlook.com>
2020-09-23 08:56:10 -0300

    first commit

---
+++ src/Tmdb.js
@@ -0,0 +1,16 @@
+const API_KEY = 'e89b8af00bc1480cb73e3c6e3d4b8ec7'
+const API_BASE = 'https://api.themoviedb.org/3'
+
+/* 
+- Originais da netflix,
+- Recomendados (trending),
+- Em alta (top rated)
+- Ação
+- Comédia
+- Romance
+- Documentarios
+*/
+
+export default{
+
+}
